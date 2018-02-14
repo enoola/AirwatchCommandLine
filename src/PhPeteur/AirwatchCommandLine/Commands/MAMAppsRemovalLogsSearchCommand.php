06fc83919e90503a7b2c3c1e86bc5970e63258de
John <john@mobilutils.com>
2018-02-14 14:13:04 +0000

    added mam-appsremovallogs-search

---
+++ src/PhPeteur/AirwatchCommandLine/Commands/MAMAppsRemovalLogsSearchCommand.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enola
+ * Date: 30/01/2018
+ * Time: 06:42
+ */
+
+namespace PhPeteur\AirwatchCommandLine\Commands;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use PhPeteur\AirwatchCommandLine\AirwatchCmd\AirwatchCmd;
+use PhPeteur\AirwatchWebservices\Services\AirwatchMAMAppsRemovalLogsSearch;
+
+class MAMAppsRemovalLogsSearchCommand extends AirwatchCmd
+{
+
+    protected function configure()
+    {
+        $this->_oAW = new AirwatchMAMAppsRemovalLogsSearch($this->_config);
+        if (is_null($this->_oAW))
+            throw new AirwatchCmdException('Unable to create AirwatchMAMAppsRemovalLogsSearch' . __CLASS__, 42);
+
+        $this->setName('mam-appsremovallogs-search');
+        if (!is_null($this->_oAW->getPossibleSearchParams())) {
+            foreach ($this->_oAW->getPossibleSearchParams() as $param => $pdescription) {
+                $this->addOption($param, null, InputOption::VALUE_REQUIRED, $pdescription);
+            }
+        }
+        $this->setDescription('Searches and retrieves removal logs for internal applications.');
+
+        parent::addGenericSearchOptions();
+    }
+
+    protected function doRun(InputInterface $input, OutputInterface $output)
+    {
+
+        $arInterestingParams = [];
+        $clPossileParam = $this->_oAW->getPossibleSearchParams();
+
+        foreach ($input->getOptions() as $optName => $optValue) {
+            if (array_key_exists($optName, $clPossileParam)) {
+                if (!is_null($optValue)) {
+                    $arInterestingParams[$optName] = $optValue;
+                }
+            }
+        }
+        $arInterestingParams = count($arInterestingParams) > 0 ? $arInterestingParams : null;
+
+        $resquery = $this->run_search($arInterestingParams, $input);
+
+        if (parent::isOptionRenderVerticalOn($input)) {
+            $this->displayVerticalSearchResults($resquery, $output);
+        } else {
+            $this->displayHorizontalSearchResults($resquery, $input, $output);
+        }
+
+        $bWeHaveResults = !is_null($resquery['data'][ $this->_oAW->getFieldnameToPickInDataResultResponse() ]);
+        $nb_entry_showed = (!$bWeHaveResults) ? '0' : count($resquery['data'][ $this->_oAW->getFieldnameToPickInDataResultResponse()]);
+        $output->writeln('I displayed : ' . $nb_entry_showed . ' result(s).');
+        if ($bWeHaveResults) {
+            $output->writeln('Current page : ' . $resquery['data']['Page'] . '.');
+            $output->writeln('Current page size : ' . $resquery['data']['PageSize'] . '.');
+            $output->writeln('Total number of entries available : ' . $resquery['data']['Total'] . '.');
+        }
+
+        return ($resquery);
+    }
+
+}
