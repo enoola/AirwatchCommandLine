65d55c37697a3d6c7c042dc8e677d61d6eb3f495
John <john@mobilutils.com>
2018-02-14 16:10:44 +0000

    added mdm-compliancepolicies-search

---
+++ src/PhPeteur/AirwatchCommandLine/Commands/MDMCompliancePoliciesSearchCommand.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enola
+ * Date: 05/02/2018
+ * Time: 13:30
+ */
+
+namespace PhPeteur\AirwatchCommandLine\Commands;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use PhPeteur\AirwatchCommandLine\AirwatchCmd\AirwatchCmd;
+use PhPeteur\AirwatchWebservices\Services\AirwatchMDMCompliancePoliciesSearch;
+
+/*
+ * Search Compliance Policy
+ * Functionality – Searches for the compliance policies with the search parameters passed.
+ */
+class MDMCompliancePoliciesSearchCommand extends AirwatchCmd
+{
+
+    protected function configure()
+    {
+        $this->_oAW = new AirwatchMDMCompliancePoliciesSearch( $this->_config );
+        if (is_null( $this->_oAW))
+            die ("Unable to create AirwatchMDMDeviceInformationsSearch object :/");
+
+        $this->setName('mdm-compliancepolicies-search');
+
+        foreach ($this->_oAW->getPossibleSearchParams() as $param => $pdescription)
+        {
+            $this->addOption($param,null, InputOption::VALUE_REQUIRED, $pdescription);
+        }
+
+        $this->setDescription('Searches for the compliance policies with the search parameters passed.');
+
+        parent::addGenericSearchOptions();
+    }
+
+    protected function doRun(InputInterface $input, OutputInterface $output) : array
+    {
+        $arInterestingParams = [];
+        $clPossileParam = $this->_oAW->getPossibleSearchParams();
+
+        foreach ($input->getOptions() as $optName => $optValue) {
+            if (array_key_exists($optName, $clPossileParam ) ) {
+                if (!is_null( $optValue ) ) {
+                    $arInterestingParams[$optName] = $optValue;
+                }
+            }
+        }
+        $arInterestingParams = ( count($arInterestingParams) > 0 ) ? $arInterestingParams : null;
+
+        $resquery = $this->run_search($arInterestingParams, $input);
+
+        if (parent::isOptionRenderVerticalOn($input)) {
+            $this->displayVerticalSearchResults($resquery, $output);
+        } else {
+            $this->displayHorizontalSearchResults($resquery, $input, $output);
+        }
+
+        $bWeHaveResults = !is_null($resquery['data'][ $this->_oAW->getFieldnameToPickInDataResultResponse() ] );
+        $nb_entry_showed = (!$bWeHaveResults ) ? '0' : count($resquery['data'][$this->_oAW->getFieldnameToPickInDataResultResponse()]);
+        $output->writeln('I displayed : '.$nb_entry_showed.' result(s).');
+        if ( $bWeHaveResults && !is_null($resquery['data']['Total']) ) {
+            $output->writeln('Current page : ' . $resquery['data']['Page'] . '.');
+            $output->writeln('Current page size : ' . $resquery['data']['PageSize'] . '.');
+            $output->writeln('Total number of entries available : ' . $resquery['data']['Total'] . '.');
+        }
+        return ( $resquery );
+    }
+}
