166848294a949a1e19789c06520aca4f8ca99eee
John <john@mobilutils.com>
2018-02-14 16:01:27 +0000

    added mdm-devices-bulksettings-search

---
+++ src/PhPeteur/AirwatchCommandLine/Commands/MDMDevicesBulkSettingsSearchCommand.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enola
+ * Date: 04/02/2018
+ * Time: 11:34
+ */
+
+namespace PhPeteur\AirwatchCommandLine\Commands;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use PhPeteur\AirwatchCommandLine\AirwatchCmd\AirwatchCmd;
+use PhPeteur\AirwatchWebservices\Services\AirwatchMDMDevicesBulkSettingsSearch;
+
+/*
+ * Retrieve Device Bulk Settings
+ * Functionality – Retrieves the values for bulk management settings done on the AirWatch Console.
+ */
+class MDMDevicesBulkSettingsSearchCommand extends AirwatchCmd
+{
+    protected function configure()
+    {
+        $this->_oAW = new AirwatchMDMDevicesBulkSettingsSearch($this->_config);
+        if (is_null($this->_oAW))
+            throw new AirwatchCmdException('Unable to create AirwatchMDMDeviceBulkSettingsSearch object within' . __CLASS__, 42);
+
+        $this->setName('mdm-devices-bulksettings-search');
+
+        $this->setDescription('Retrieves the values for bulk management settings done on the AirWatch Console.');
+
+        parent::addGenericSearchOptions();
+    }
+
+    protected function doRun(InputInterface $input, OutputInterface $output) : array
+    {
+
+        $arInterestingParams = null;
+
+        $resquery = $this->run_search_custo($arInterestingParams, $input);
+
+        if (parent::isOptionRenderVerticalOn($input)) {
+            $this->displayVerticalSearchResults($resquery, $output);
+        } else {
+            $this->displayHorizontalSearchResults($resquery, $input, $output);
+        }
+
+        $bWeHaveResults = !is_null($resquery['data'][ $this->_oAW->getFieldnameToPickInDataResultResponse() ] );
+        $nb_entry_showed = (!$bWeHaveResults ) ? '0' : count($resquery['data'][$this->_oAW->getFieldnameToPickInDataResultResponse()]);
+        $output->writeln('I displayed : '.$nb_entry_showed.' result(s).');
+        if ( $bWeHaveResults && !is_null($resquery['data']['Total']) ) {
+            $output->writeln('Current page : ' . $resquery['data']['Page'] . '.');
+            $output->writeln('Current page size : ' . $resquery['data']['PageSize'] . '.');
+            $output->writeln('Total number of entries available : ' . $resquery['data']['Total'] . '.');
+        }
+        return ( $resquery );
+    }
+}
