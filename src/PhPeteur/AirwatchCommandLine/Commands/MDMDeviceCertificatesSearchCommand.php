1f959b6d9d166365c32e3c6a34e1d2614d20c41a
John <john@mobilutils.com>
2018-02-14 16:02:35 +0000

    added mdm-device-certificates-search

---
+++ src/PhPeteur/AirwatchCommandLine/Commands/MDMDeviceCertificatesSearchCommand.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enola
+ * Date: 04/02/2018
+ * Time: 13:01
+ */
+
+namespace PhPeteur\AirwatchCommandLine\Commands;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use PhPeteur\AirwatchCommandLine\AirwatchCmd\AirwatchCmd;
+use PhPeteur\AirwatchWebservices\Services\AirwatchMDMDeviceCertificatesSearch;
+
+/*
+ * Retrieve Certificate Details From the Device
+ * Functionality – Retrieves the details of the certificates that are present on the device.
+ */
+class MDMDeviceCertificatesSearchCommand extends AirwatchCmd
+{
+    protected function configure()
+    {
+        $this->_oAW = new AirwatchMDMDeviceCertificatesSearch($this->_config);
+        if (is_null($this->_oAW))
+            die ("Unable to create AirwatchMDMDeviceSecuritySearch object :/");
+
+        $this->setName('mdm-device-certificates-search');
+
+        foreach ($this->_oAW->getPossibleSearchParams() as $param => $pdescription) {
+            $this->addOption($param, null, InputOption::VALUE_REQUIRED, $pdescription);
+        }
+
+        $this->setDescription('Retrieves the details of the certificates that are present on the device.');
+
+        parent::addGenericSearchOptions();
+    }
+
+    protected function doRun(InputInterface $input, OutputInterface $output): array
+    {
+        $arInterestingParams = [];
+        $clPossileParam = $this->_oAW->getPossibleSearchParams();
+
+        if (is_null($input->getOption('id')))
+            throw new \Exception("I need an id at least");
+
+        $arPossibleSearchBy = ["Macaddress", "Udid", "Serialnumber", "ImeiNumber"];
+        if (!is_null($input->getOption('searchby'))) {
+            if (!in_array($input->getOption('searchby'), $arPossibleSearchBy))
+                throw new \Exception('Possible value for searchby :' . implode(',', $arPossibleSearchBy) . '.');
+        }
+
+        foreach ($input->getOptions() as $optName => $optValue) {
+            if (array_key_exists($optName, $clPossileParam)) {
+                if (!is_null($optValue)) {
+                    $arInterestingParams[$optName] = $optValue;
+                }
+            }
+        }
+        $arInterestingParams = (count($arInterestingParams) > 0) ? $arInterestingParams : null;
+
+        $resquery = $this->run_search($arInterestingParams, $input);
+
+        if (parent::isOptionRenderVerticalOn($input)) {
+            $this->displayVerticalSearchResults($resquery, $output);
+        } else {
+            $this->displayHorizontalSearchResults($resquery, $input, $output);
+        }
+
+        $bWeHaveResults = !is_null($resquery['data'][$this->_oAW->getFieldnameToPickInDataResultResponse()]);
+        $nb_entry_showed = (!$bWeHaveResults) ? '0' : count($resquery['data'][$this->_oAW->getFieldnameToPickInDataResultResponse()]);
+        $output->writeln('I displayed : ' . $nb_entry_showed . ' result(s).');
+        if ($bWeHaveResults && !is_null($resquery['data']['Total'])) {
+            $output->writeln('Current page : ' . $resquery['data']['Page'] . '.');
+            $output->writeln('Current page size : ' . $resquery['data']['PageSize'] . '.');
+            $output->writeln('Total number of entries available : ' . $resquery['data']['Total'] . '.');
+        }
+        return ($resquery);
+    }
+
+
+
+}
