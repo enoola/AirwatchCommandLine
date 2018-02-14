f34eb9d629d864773b7087799c16cc4b53e51d4e
John <john@mobilutils.com>
2018-02-14 15:40:25 +0000

    added system-group-search

---
+++ src/PhPeteur/AirwatchCommandLine/Commands/SystemGroupSearchCommand.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enola
+ * Date: 03/02/2018
+ * Time: 12:07
+ */
+
+namespace PhPeteur\AirwatchCommandLine\Commands;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use PhPeteur\AirwatchWebservices\Services\AirwatchSystemGroupSearch;
+use PhPeteur\AirwatchCommandLine\AirwatchCmd\AirwatchCmd;
+
+
+/*
+ * Fetch Organization Group's Details
+ * Functionality – Retrieves the details of the organization group.
+ */
+class SystemGroupSearchCommand extends AirwatchCmd
+{
+    protected function configure()
+    {
+        $this->_oAW = new AirwatchSystemGroupSearch($this->_config);
+        if (is_null($this->_oAW))
+            throw new AirwatchCmdException('unable to create AirwatchMDMSmartGroupSearch object within' . __CLASS__, 42);
+
+        $this->setName('system-group-search');
+        if (!is_null($this->_oAW->getPossibleSearchParams())) {
+            foreach ($this->_oAW->getPossibleSearchParams() as $param => $pdescription) {
+                $this->addOption($param, null, InputOption::VALUE_REQUIRED, $pdescription);
+
+            }
+        }
+        $this->setDescription('Retrieves the details of the organization group.');
+
+        parent::addGenericSearchOptions();
+    }
+
+    protected function doRun(InputInterface $input, OutputInterface $output) : array
+    {
+        $arInterestingParams = [];
+        $clPossileParam = $this->_oAW->getPossibleSearchParams();
+
+        if (is_null ( $input->getOption('id')) )
+            throw new \Exception("I need an id at least");
+
+        foreach ($input->getOptions() as $optName => $optValue) {
+            if (array_key_exists($optName, $clPossileParam ) ) {
+                if (!is_null( $optValue ) ) {
+                    $arInterestingParams[$optName] = $optValue;
+                }
+            }
+        }
+        $arInterestingParams = ( count($arInterestingParams) > 0 ) ? $arInterestingParams : null;
+
+        $resquery = $this->run_search_custo($arInterestingParams, $input);
+
+        if (parent::isOptionRenderVerticalOn($input)) {
+            $this->displayVerticalSearchResults($resquery, $output);
+        } else {
+            $this->displayHorizontalSearchResults($resquery, $input, $output);
+        }
+
+        $bWeHaveResults = !is_null($resquery['data'][ $this->_oAW->getFieldnameToPickInDataResultResponse() ] );
+        $nb_entry_showed = (!$bWeHaveResults ) ? '0' : count($resquery['data'][$this->_oAW->getFieldnameToPickInDataResultResponse()]);
+        $output->writeln('I displayed : '.$nb_entry_showed.' result(s).');
+        if ( $bWeHaveResults && !is_null($resquery['data']['Total']) ) {
+            $output->writeln('Current page : ' . $resquery['data']['Page'] . '.');
+            $output->writeln('Current page size : ' . $resquery['data']['PageSize'] . '.');
+            $output->writeln('Total number of entries available : ' . $resquery['data']['Total'] . '.');
+        }
+        return ( $resquery );
+    }
+
+
+
+}
