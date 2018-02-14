c842eb7a386b013fb6430272dae24437fc4eb709
John <john@mobilutils.com>
2018-02-14 12:54:55 +0000

    added mam-apps-playstore-search

---
+++ src/PhPeteur/AirwatchCommandLine/Commands/MAMAppsPlaystoreSearchCommand.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enola
+ * Date: 15/01/2018
+ * Time: 20:01
+ */
+
+namespace PhPeteur\AirwatchCommandLine\Commands;
+
+
+use PhPeteur\AirwatchCommandLine\AirwatchCmd\AirwatchCmd;
+use PhPeteur\AirwatchWebservices\Services\AirwatchMAMAppsPlayStoreSearch;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\Table;
+
+class MAMAppsPlaystoreSearchCommand extends AirwatchCmd
+{
+
+    public function configure()
+    {
+        parent::configure();
+        $this->setName('mam-apps-playstore-search')
+            ->addArgument('appname', InputArgument::REQUIRED, 'appname to search in playstore')
+            ->setDescription("gives access to aw mam apps playstore search");
+
+        $this->_oAW = new AirwatchMAMAppsPlayStoreSearch( $this->_config );
+    }
+
+    protected function doRun(InputInterface $input, OutputInterface $output)
+    {
+        $res = $this->_oAW->Search( $input->getArgument('appname') );
+        if (is_null( $this->_oAW))
+            die ("Unable to create AirwatchMAMAppsPlayStoreSearch object :/");
+
+        if (array_key_exists('Applications', $res['data']) )
+        {
+            $table = new Table($output);
+            $table->setHeaders(['BundleID', 'ApplicationName', 'CurrentVersion']);
+            foreach ($res['data']['Applications'] as $k => $arOneResult) {
+                $table->addRow($arOneResult);
+            }
+            $table->render();
+
+            $output->writeln('Total number of results displayed : ' . count($res['data']['Applications']) . '.');
+
+        } else {
+            $output->writeln('<red>No results.</red>');
+        }
+
+        return;
+    }
+
+}
